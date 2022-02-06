1926d3c414432484b2d0402a45043be0b90734c2
Konrad Gajewski <konrad.gajewski@physics.uu.se>
2022-02-06 12:11:15 +0100

    Version 1.0.0

--- cmds/st.cmd
+++ cmds/st.cmd
@@ -2,4 +2,5 @@
 
 require vat616
 
-iocshLoad(${vat616_DIR}/vat616.iocsh, "IOCNAME = ioc-vat616, IP_ADDR = 192.168.10.126")
+#iocshLoad(${vat616_DIR}/vat616.iocsh, "IOCNAME = ioc-vat616, IP_ADDR = 192.168.10.126")
+iocshLoad(${vat616_DIR}/vat616.iocsh, "IOCNAME = ioc-vat616, IP_ADDR = 192.168.10.117")
