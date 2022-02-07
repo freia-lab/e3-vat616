3783217ec1ba13fd9cd24978a5cf677aa257abd9
Konrad Gajewski <konrad.gajewski@physics.uu.se>
2022-02-07 16:30:05 +0100

    Changed the protocol. Updated the epics database and startup scripts. Updated opi.

--- cmds/st.cmd
+++ cmds/st.cmd
@@ -2,5 +2,9 @@
 
 require vat616
 
-#iocshLoad(${vat616_DIR}/vat616.iocsh, "IOCNAME = ioc-vat616, IP_ADDR = 192.168.10.126")
-iocshLoad(${vat616_DIR}/vat616.iocsh, "IOCNAME = ioc-vat616, IP_ADDR = 192.168.10.117")
+iocshLoad(${vat616_DIR}/vat616_server.iocsh, "IOCNAME = ioc-vat616, IP_ADDR = 192.168.10.126")
+
+iocshLoad(${vat616_DIR}/vat616_valve.iocsh, "PREFIX=CstatV-RHtr, V=CV580, INDX=0")
+iocshLoad(${vat616_DIR}/vat616_valve.iocsh, "PREFIX=CstatV-RHtr, V=CV581, INDX=1")
+iocshLoad(${vat616_DIR}/vat616_valve.iocsh, "PREFIX=CstatV-RHtr, V=CV582, INDX=2")
+iocshLoad(${vat616_DIR}/vat616_valve.iocsh, "PREFIX=CstatV-RHtr, V=CV583, INDX=3")
--- tools/vat616sim.sh
+++ tools/vat616sim.sh
@@ -6,19 +6,40 @@
 noerr="0"
 errno="5"
 syserr="-1"
+IFS=' '
 
 function cmd_handle {
 
-    if [[ "$cmd" == "L?" ]]; then
-	echo "L= 0 0.0 1 1.1 2 2.2 -3 100"
+    if [[ "$cmd" == "L? 0" ]]; then
+	echo "L= 0 0 0 1 0.11"
 
-    elif [[ "$cmd" == "R "[0-1] ]]; then
-	echo "$cmd $noerr"
+    elif [[ "$cmd" == "L? 1" ]]; then
+	echo "L= 1 1 5 1 2.2"
 
-    elif [[ "$cmd" == "R "[2-3] ]]; then
-	echo "$cmd $syserr"
+    elif [[ "$cmd" == "L? 2" ]]; then
+	echo "L= 2 -1 0 1 3.3"
 
+    elif [[ "$cmd" == "L? 3" ]]; then
+	echo "L= 3 0 0 0 4.4"
+
+    elif [[ "$cmd" == "Rem "[0-1] ]]; then
+	read c i <<<"$cmd"
+	echo "CS $i $noerr"
+
+    elif [[ "$cmd" == "Rem "[2-3] ]]; then
+	read c i <<<"$cmd"
+	echo "CS $i $syserr"
+	
+    elif [[ "$cmd" == "Loc "[1-2] ]]; then
+	read c i <<<"$cmd"
+	echo "CS $i $noerr"
+
+    elif [[ "$cmd" == "Loc "[03] ]]; then
+	read c i <<<"$cmd"
+	echo "CS $i $noerr"
+	
     else
+	echo "? $cmd"
         echo -e "`date '+%H%M%S'`\tunknown command '$cmd'" 1>&2
         return
     fi
