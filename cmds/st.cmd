bb48ba6ed36782509f7631c02ea8d9386d336b12
Konrad Gajewski <konrad.gajewski@physics.uu.se>
2022-03-18 14:36:58 +0100

    New epics base.

--- cmds/st.cmd
+++ cmds/st.cmd
@@ -2,11 +2,15 @@
 
 require vat616
 
-iocshLoad(${vat616_DIR}/vat616_server.iocsh, "IOCNAME = ioc-vat616, IP_ADDR = 192.168.10.117")
+iocshLoad(${vat616_DIR}/vat616_server.iocsh, "IOCNAME = ioc-vat616, IP_ADDR = 192.168.10.110")
 
 iocshLoad(${vat616_DIR}/vat616_valve.iocsh, "PREFIX=CstatV-RHtr, V=CV580, INDX=0")
-#iocshLoad(${vat616_DIR}/vat616_valve.iocsh, "PREFIX=CstatV-RHtr, V=CV581, INDX=1")
-#iocshLoad(${vat616_DIR}/vat616_valve.iocsh, "PREFIX=CstatV-RHtr, V=CV582, INDX=2")
-#iocshLoad(${vat616_DIR}/vat616_valve.iocsh, "PREFIX=CstatV-RHtr, V=CV583, INDX=3")
+iocshLoad(${vat616_DIR}/vat616_valve.iocsh, "PREFIX=CstatV-RHtr, V=CV581, INDX=1")
+iocshLoad(${vat616_DIR}/vat616_valve.iocsh, "PREFIX=CstatV-RHtr, V=CV582, INDX=2")
+iocshLoad(${vat616_DIR}/vat616_valve.iocsh, "PREFIX=CstatV-RHtr, V=CV583, INDX=3")
 
 afterInit (dbpf CstatV-RHtr:CV580:sErrCode.SCAN "1 second")
+afterInit (dbpf CstatV-RHtr:CV581:sErrCode.SCAN "1 second")
+afterInit (dbpf CstatV-RHtr:CV582:sErrCode.SCAN "1 second")
+afterInit (dbpf CstatV-RHtr:CV583:sErrCode.SCAN "1 second")
+
